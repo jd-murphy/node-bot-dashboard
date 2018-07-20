b453728883c418877df049344f88ec66ab3c07d6
Jordan Murphy <jordan@blueberri-saas.com>
2018-07-20 14:29:57 -0500

    serve static file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,10 +10,35 @@ app.get('/', (req, res) => {
     res.send("Node server live!");
 });
 app.get('/dashboard', (req, res) => {
-    res.send("/index.html");
+    serve_static_file('index.html');
 });
+
 
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
 });
+
+
+
+
+
+function serve_static_file(file, res) {
+    var rs = fs.createReadStream(file);
+    res.writeHead(200, {"Content-Type": 'text/html'});
+    
+    rs.on('readable', () => {
+       var d = rs.read();
+       if (d) {
+           if (typeof d === 'string') {
+               res.write(d);
+           } else if (typeof d === 'object' && d instanceof Buffer) {
+               res.write(d.toString('utf8'));
+           }
+       } 
+    });
+
+    rs.on('end', () => {
+        res.end();
+    })
+}
