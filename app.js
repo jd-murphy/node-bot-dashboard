7a0aafac578b4ad46d3db94b621dacbb4deeb5e5
Jordan Murphy <jordan@blueberri-saas.com>
2018-07-21 01:52:13 -0500

    res.sendFile

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/dashboard', (req, res) => {
-    serve_static_file('index.html', res);
+    res.sendFile('index.html');
 });
 
 app.get('/getLogs', async (req, res) => {
@@ -77,22 +77,22 @@ function setUpFirebase() {
 
 
 
-function serve_static_file(file, res) {
-    var rs = fs.createReadStream(file);
-    res.writeHead(200, {"Content-Type": 'text/html'});
+// function serve_static_file(file, res) {
+//     var rs = fs.createReadStream(file);
+//     res.writeHead(200, {"Content-Type": 'text/html'});
     
-    rs.on('readable', () => {
-       var d = rs.read();
-       if (d) {
-           if (typeof d === 'string') {
-               res.write(d);
-           } else if (typeof d === 'object' && d instanceof Buffer) {
-               res.write(d.toString('utf8'));
-           }
-       } 
-    });
+//     rs.on('readable', () => {
+//        var d = rs.read();
+//        if (d) {
+//            if (typeof d === 'string') {
+//                res.write(d);
+//            } else if (typeof d === 'object' && d instanceof Buffer) {
+//                res.write(d.toString('utf8'));
+//            }
+//        } 
+//     });
 
-    rs.on('end', () => {
-        res.end();
-    })
-}
+//     rs.on('end', () => {
+//         res.end();
+//     })
+// }
