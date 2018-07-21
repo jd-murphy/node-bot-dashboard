828538368c258ebc81f81da948670bdb52bdd16e
Jordan Murphy <jordan@blueberri-saas.com>
2018-07-20 22:48:00 -0500

    loop through firebase results

--- firebaseWorker.js
+++ firebaseWorker.js
@@ -10,32 +10,21 @@ config = {
     "projectId": process.env.PROJECTID,
     "storageBucket": process.env.STORAGEBUCKET,
     "messagingSenderId": process.env.MESSAGINGSENDERID,
-    "serviceAccount": process.env.SERVICEACCOUNT // was sapath
+    "serviceAccount": process.env.SERVICEACCOUNT 
   }
 
 
 
 function setUpServiceAccountFile() {
     console.log("setUpServiceAccountFile()")
-    // fs.writeFile(process.env.SAPATH, process.env.SERVICEACCOUNT,  'utf8', function(err) {
-    //     console.log("writing file...")
-    //     if(err) {
-    //         console.log("ERROR writing file!!!")
-    //         return console.log(err);
-    //     } else {
-            // console.log("No error, initializing firebase.")
             admin.initializeApp({
                 credential: admin.credential.cert(JSON.parse(serviceAccount)),
                 databaseURL: 'https://twilio-bot-1601d.firebaseio.com/'
                 });
-                console.log("firebase initialized!");
-                getLogs();
+            console.log("firebase initialized!");
+            getLogs();
         }
     
-        
-    // });
-    
-    // }
 
 
 function getLogs() {
@@ -45,9 +34,13 @@ function getLogs() {
     console.log("connecting to firebase!");
     // Attach an asynchronous callback to read the data at our posts reference
     ref.on("value", function(snapshot) {
-      console.log("HERE IS THE SNAPSHOT FROM FIREBASE ->  " + snapshot.val());
+        console.log("HERE IS THE SNAPSHOT FROM FIREBASE ->  ");
+        snapshot.val().forEach(element => {
+            console.log(element.date)
+            console.log(element.info)
+        });
     }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
+        console.log("The read failed: " + errorObject.code);
     });
     console.log("firebase read finished!");
 }
