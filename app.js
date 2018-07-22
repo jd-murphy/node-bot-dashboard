a08d4ef9cfefb14d75b5523fd5ac45e85b901923
Jordan Murphy <jordan@blueberri-saas.com>
2018-07-22 14:20:09 -0500

    changed res.send() for /api/bot-report

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/api/bot-report', (req, res) => {
     print("printing req ->")
     print(str(req))
     io.emit('botAlert', 'testing!!!');
-    res.send({status: "received" });
+    res.send("/api/bot-report has received your request!");
 });
 
 
