62f93e3538f39a48f168a4e8c8e688f63d3161dc
Jordan Murphy <jordan@blueberri-saas.com>
2018-08-02 21:31:02 -0500

    update bootstrap to v4

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use('/images/', express.static('./images'));
 
+
 app.get('/', (req, res) => {
     res.send("Error 404: Hmmmm, Sorry, I can't find the page you're looking for. Maybe you meant http://bit.ly/EXRaidsBCS ?");
 });
